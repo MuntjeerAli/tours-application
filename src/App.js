cc7938169bf0b5b5492a756a12d827094257e406
MuntjeerAli <muntjeersayyad@gmail.com>
2022-06-23 10:10:13 +0530

    Tour project completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
+import React, {useState, useEffect} from 'react';
+import Loading from './components/Loading';
+import Tours from './components/Tours';
+const url = "https://course-api.com/react-tours-project";
 
-function App() {
+const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const updatedTours = tours.filter((tour) => tour.id !== id);
+    setTours(updatedTours);
+  }
+
+  const fetchTours = async() => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setIsLoading(false);
+      setTours(tours);
+    } catch(error) {
+      setIsLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect (() => {
+    fetchTours();
+  }, [])
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={() => fetchTours()}>Reload</button>
+        </div>
+      </main>
+    )
+  }
+
   return (
     <>
-      <h1>Hello React</h1>
+      <Tours tours = {tours} removeTour={removeTour}/>
     </>
   );
 }
